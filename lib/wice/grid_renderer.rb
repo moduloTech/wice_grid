6ab7d0019bf8bca16ce639b715a8b4e42d2070ba
Matthieu "Ezveus" Ciappara <matthieu.ciappara@outlook.com>
2023-04-07 17:27:32 +0200

    Replace fa-file-csv with fa-file-excel-o

--- lib/wice/grid_renderer.rb
+++ lib/wice/grid_renderer.rb
@@ -106,7 +106,7 @@ module Wice
     def csv_export_icon #:nodoc:
       content_tag(
         :div,
-        content_tag(:i, '', class: 'fa fa-file-csv'),
+        content_tag(:i, '', class: 'fa fa-file-excel-o'),
         title: NlMessage['csv_export_tooltip'],
         class: 'clickable export-to-csv-button'
       )
